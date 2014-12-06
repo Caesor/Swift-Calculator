d45174fc20dd1ea178459c020621e3256662eb71
caesor <liaozksysu@gmail.com>
2014-12-06 12:53:26 +0800

    version 1.0

---
+++ AdvancedCalculator/ViewController.swift
@@ -0,0 +1,190 @@
+//
+//  ViewController.swift
+//  AdvancedCalculator
+//
+//  Created by zekai on 4/12/14.
+//  Copyright (c) 2014 zekai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var bigResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    var infix:String = ""
+    var postfix = [String]()
+    
+    @IBAction func didClicked(sender: UIButton) {
+        let value = sender.currentTitle
+        
+        if bigResult.text != ""{
+            bigResult.text = ""
+        }
+//        print(value)
+        if value == "=" {
+            var result = calculate()
+            if result == ""{
+                resultLabel.text = "This's not a good Math expression\n"
+            }
+            else{
+                bigResult.text = "\(result)"
+                infix = "\(result)"
+            }
+            return
+        }
+        else if value == "c"{
+            infix.removeAll()
+        }
+        else if value == "del"{
+            if countElements(infix) > 0{
+                infix = dropLast(infix)
+                print(infix)
+            }
+        }
+        else{
+            infix += value!
+        }
+        //update the information on blackboard
+        if countElements(infix) > 0 {
+            resultLabel.text = "\(infix)"
+        }
+        else{
+            resultLabel.text = "0"
+        }
+    }
+
+  /**
+    * To judge the priority of a operator
+    *
+    * @retrun : the priority of operator
+    */
+    func checkPro(op:String) -> Int{
+        if op == "+" || op == "-"{
+            return 1
+        }
+        else if op == "*" || op == "/"{
+            return 2
+        }
+        else{
+            return 3
+        }
+    }
+    
+  /**
+    * Calculate the math expresion according to Postfix Expression
+    *
+    * @retrun : a string of result
+    */
+    func calculate() ->String{
+        var solution:String = ""
+        if inToPost(){
+            print("Postfix: \(postfix)\n")
+            var result:Double = 0
+            //use a String array to simulate "Stack"
+            var stack = [String]()
+            
+            for eachParam in postfix{
+                if eachParam == "+" || eachParam == "-" || eachParam == "*" || eachParam == "/"{
+                    var param2 = NSString(string: stack.last!).doubleValue
+                    stack.removeLast()
+                    var param1 = NSString(string: stack.last!).doubleValue
+                    stack.removeLast()
+                    switch eachParam{
+                        case "+":
+                            result = param1 + param2
+                        case "-":
+                            result = param1 - param2
+                        case "*":
+                            result = param1 * param2
+                        case "/":
+                            result = param1 / param2
+                        default:
+                            result = 0
+                    }
+                    stack.append("\(result)")
+                }
+                else{
+                    stack.append(eachParam)
+                }
+            }
+            solution = stack.last!
+            print("\(stack.last)")
+        }
+        else{
+            print("This's not a good Math expression\n")
+            solution = ""
+        }
+        return solution
+    }
+    
+    /**
+     * Transform the math expresstion from Nifix Expression to Postfix Expression
+     *
+     * @retrun : a bool, True means math expression is correct, otherwise False
+     */
+    func inToPost() -> Bool{
+        var param:String = ""
+        //used to store the operators
+        var opert = [String]()
+        
+        var index = 0
+        postfix.removeAll()
+        
+        for eachChar in infix {
+            if eachChar == "+" || eachChar == "-" || eachChar == "*" || eachChar == "/"{
+                if param != ""{
+                    postfix.append(param)
+                    param = ""
+                }
+                else{//the whole string is not a math expression
+                    return false
+                }
+                //To judge pop or not
+                while  opert.count > 0 {
+                    if checkPro(opert.last!) >= checkPro(String(eachChar)){
+                        //pop stack and push to postfix array
+                        postfix.append(opert.last!)
+                        opert.removeLast()
+                    }
+                    else{
+                        break
+                    }
+                }
+                opert.append("\(eachChar)")
+            }
+            else{
+                param += "\(eachChar)"
+            }
+            ++index
+            //at the end of infix
+            if index == countElements(infix){
+                //the whole string is not a math expression
+                if param == ""{
+                    return false
+                }
+                postfix.append(param)
+                while opert.count > 0 {
+                    postfix.append(opert.last!)
+                    opert.removeLast()
+                }
+            }
+        }
+        return true
+    }
+       
+}
+
+
